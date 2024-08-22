ec9b70ccbe35c36184a7d3fa912f1f91217eaf7b
franciscolir <francisco.lira.b@gmail.com>
2024-08-22 12:34:08 -0400

    first commit

---
+++ src/main/java/com/api/documentacion/DocumentacionApplication.java
@@ -0,0 +1,13 @@
+package com.api.documentacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/documentacion/DocumentacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.documentacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
