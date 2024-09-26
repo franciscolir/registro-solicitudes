96c1ed5a1cdadda0c2779c2c602a6e6ef1083757
franciscolir <francisco.lira.b@gmail.com>
2024-09-26 13:04:10 -0300

    agrega widget fecha y ultimo memo. inicia tetst de colores

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,51 +1,42 @@
 //VALORES POR API #####################################
 document.addEventListener('DOMContentLoaded', () => {
-    const tableBody = document.querySelector('#valores');
-    const fechaDiv = document.getElementById('fecha');
-    const fecha = fechaDiv.textContent || fechaDiv.innerText;
+    const valorContainer = document.querySelector('#valores');
+    const fecha = new Date();
+    const day = String(fecha.getDate()).padStart(2, '0');
+    const month = String(fecha.getMonth() + 1).padStart(2, '0');
+    const year = fecha.getFullYear();
+    const formattedDate = `${day}-${month}-${year}`;
 
-    const loadData9 = () => {
-    // Reemplaza esta URL con la URL de tu API
-    
-    const apiUrl = `https://mindicador.cl/api/utm/${fecha}`;
+    const loadData = () => {
+        // Reemplaza esta URL con la URL de tu API
+        const apiUrl = `https://mindicador.cl/api/utm/${formattedDate}`;
 
-
-    // Solicitar datos usando Axios
-    axios.get(apiUrl)
-   
-        .then(response => {
-            // La respuesta contiene los datos en response.data
-            const nombre = response.data.codigo
-            const registros = response.data.serie;
-            console.log('Datos recibidos tabla:', response);
-         
-            // Limpiar el cuerpo de la tabla
-            tableBody.innerHTML = '';
-
-            // Crear filas para cada 
-            
-            registros.forEach(registro => {
-                console.log(registro)
-                const row = document.createElement('table');
-
-                row.innerHTML = `
-
-                        <td>
-                        ${registro.valor}
-                        </td>
-                `;
-
-                tableBody.appendChild(row);
-            });
-        })
-        .catch(error => {
-            console.error('Error al obtener los datos:', error);
-        });
         // Solicitar datos usando Axios
-}
+        axios.get(apiUrl)
+            .then(response => {
+                const registros = response.data.serie;
+                console.log('Datos recibidos:', response);
 
-loadData9();
+                // Limpiar el contenido previo
+                valorContainer.innerHTML = '';
+
+                // Mostrar solo el último registro
+                if (registros.length > 0) {
+                    const valor = registros[registros.length - 1].valor; // Obtiene el último valor
+                    valorContainer.innerHTML = `$${valor}`;
+                } else {
+                    valorContainer.innerHTML = 'No hay datos disponibles';
+                }
+            })
+            .catch(error => {
+                console.error('Error al obtener los datos:', error);
+                valorContainer.innerHTML = 'Error al cargar datos';
+            });
+    }
+
+    loadData();
 });
+
 
 //FUNCION CAPTA PARAMETROS DEL ACORDEON ####################
 function captarParametros(id,numeroSolicitud,nombreModal,nombreDiv) {
@@ -156,7 +147,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const loadData2 = () => {
         // Reemplaza esta URL con la URL de tu API
-        const apiUrl = 'http://localhost:8080/eventos?size=4';
+        const apiUrl = 'http://localhost:8080/eventos?size=2';
     
         // Solicitar datos usando Axios
         axios.get(apiUrl)
@@ -197,14 +188,12 @@ const invitadosString = (registro.invitado && registro.invitado.length > 2)
                     row.innerHTML = `
                     
                     
-                            <th>
-                            ${tipoFormatted}  |  ${registro.fecha}
-                            <th>
+                            <th class="th"> ${tipoFormatted}  |  ${registro.fecha} <th>
                             <tr> 
-                            ${registro.descripcion}<br>
-                            Recinto: ${registro.establecimiento}<br>
-                            Invitados: ${invitadosString}
-                            </tr>
+                                ${registro.descripcion}<br>
+                                Recinto: ${registro.establecimiento}<br>
+                                Invitados: ${invitadosString}
+                            </td>
           
                     `;
     
@@ -221,187 +210,33 @@ const invitadosString = (registro.invitado && registro.invitado.length > 2)
 
 //ULTIMA RESPUESTA###################################
 document.addEventListener('DOMContentLoaded', () => {
-    const accordionContainer = document.getElementById('accordionUltimaRespuesta');
-    const loadData = () => {
-  
-    // Solicitar datos usando Axios y los ordena con desc y limita la selecion a 1
-    axios.get('http://localhost:8080/respuestas?size=1&sort=id,desc')
-        .then(response => {
-            // La respuesta contiene los datos en response.data
+    const ultimaRespuesta = document.getElementById('ultimaRespuesta');
 
-                // Asegúrate de que los datos son un objeto y contiene `items`
-                const data = response.data;
-               console.log('Datos recibidos ultimo memo:', data.content);
+    const loadDataUltimaRespuesta = () => {
+        // Solicitar datos usando Axios y los ordena con desc y limita la seleccion a 1
+        axios.get('http://localhost:8080/respuestas?size=1&sort=id,desc')
+            .then(response => {
+                // Asegúrate de que los datos son un objeto y contiene `content`
+                const data = response.data.content;
 
-                // Accede al array `items` dentro del objeto
-                const items = data.content;
+                if (Array.isArray(data) && data.length > 0) {
+                    // Limpiar el contenedor antes de mostrar nuevos datos
+                    ultimaRespuesta.innerHTML = '';
 
-                if (Array.isArray(items)) {
-                    accordionContainer.innerHTML = '';
-
-                    items.forEach((item, index) => {
-                      
-                        const accordionItem = `
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="heading${index}">
-                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseA${index}" aria-expanded="true" aria-controls="collapse${index}">
-                                        ${item.numeroRespuesta} 
-                                    </button>
-                                </h2>
-                                <div id="collapseA${index}" class="accordion-collapse collapse" aria-labelledby="heading${index}" data-bs-parent="#accordionUltimaRespuesta">    
-                                    
-                                    <div class="accordion-body">
-                                            <div class="accordion-items">
-                                                    - ${item.titulo}<br>
-                                                    - Fecha ${item.fechaRespuesta} <br>
-                                            </div>
-                                    </div>
-                                </div>
-                            </div>
-                        `;
-
-                        accordionContainer.innerHTML += accordionItem;
-                    });
+                    // Mostrar el primer item (última respuesta)
+                    const item = data[0];
+                    ultimaRespuesta.innerHTML = `${item.numeroRespuesta}`;
                 } else {
-                    console.error('La propiedad `items` no es un array.');
+                    console.error('La propiedad `content` no es un array o está vacío.');
+                    ultimaRespuesta.innerHTML = '<p>No se encontraron respuestas.</p>';
                 }
-                
             })
             .catch(error => {
                 console.error('Error:', error);
                 alert('No se pudo cargar los datos. Verifica la URL y la conexión a Internet.');
             });
-        }
-        loadData();
+    };
+
+    loadDataUltimaRespuesta();
 });
-
-//CALENDAR##############################################
-const obtenerFechaHoy = () => {
-    const hoy = new Date();
-    const dia = String(hoy.getDate()).padStart(2, '0');
-    const mes = String(hoy.getMonth() + 1).padStart(2, '0'); // Mes es 0-11
-    const anio = hoy.getFullYear();
-    return `${dia}-${mes}-${anio}`;
-};
-document.getElementById('fecha').textContent = obtenerFechaHoy();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-//TABLA SOLICITUDES#########################################
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleTableBtn = document.getElementById('toggleTableBtn');
-    const solicitudesTable = document.getElementById('solicitudes-table');
-    const tableBody = solicitudesTable.querySelector('tbody');
-    const paginationContainer = document.getElementById('pagination-container');
-    const pagination = document.getElementById('pagination');
-    
-    let isTableVisible = false;
-    let currentPage = 0;
-    const pageSize = 10;
-
-    function fetchData(page) {
-
-        axios.get(`http://localhost:8080/solicitudes?page=${page}&size=${pageSize}`)
-            .then(response => {
-                const registros = response.data.content;
-                const totalPages = response.data.totalPages;
-
-
-            tableBody.innerHTML = '';
-
-                // Render table data
-                registros.forEach(registro => {
-                    const estadoFormatted = registro.estado
-                    .toLowerCase()                  // Convierte todo el texto a minúsculas
-                    .replace(/_/g, ' ')             // Reemplaza guiones bajos con espacios
-                    //.replace(/^(.)/, (match, p1) => p1.toUpperCase()); // Convierte la primera letra a mayúscula
-                    .replace(/\b\w/g, letra => letra.toUpperCase());
-    
-                    const row = document.createElement('tr');
-                    row.innerHTML = `
-                        <td>${registro.numeroSolicitud}</td>
-                        <td>${registro.emisor}</td>
-                        <td>${registro.titulo}</td>
-                        <td>${registro.descripcion}</td>
-                        <td>${registro.fechaSolicitud}</td>
-                        <td>${registro.fechaIngresoDepartamento}</td>
-                        <td>${estadoFormatted}</td>
-                    `;
-                    tableBody.appendChild(row);
-                });
-
-                // Render pagination
-                pagination.innerHTML = '';
-                for (let i = 0; i < totalPages; i++) {
-                    const pageItem = document.createElement('li');
-                    pageItem.className = 'page-item' + (i === page ? ' active' : '');
-                    pageItem.innerHTML = `<a class="page-link" href="#" data-page="(${i})">${i + 1}</a>`;
-                    pagination.appendChild(pageItem);
-                }
-             
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
-    }
-
-    function fetchPage(page) {
-        currentPage = page;
-        fetchData(page);
-    }
-
-    function toggleTableVisibility() {
-        if (isTableVisible) {
-            // Ocultar la tabla y la paginación
-            solicitudesTable.style.display = 'none';
-            paginationContainer.style.display = 'none';
-            toggleTableBtn.textContent = 'Mostrar';
-        } else {
-            // Mostrar la tabla y la paginación
-            solicitudesTable.style.display = 'table';
-            paginationContainer.style.display = 'block';
-            fetchData(currentPage);
-            toggleTableBtn.textContent = 'Ocultar';
-        }
-        isTableVisible = !isTableVisible;
-    }
-
-    // Asignar la función al evento click del botón
-    toggleTableBtn.addEventListener('click', toggleTableVisibility);
-   
-    pagination.addEventListener('click', function(event) {
-        const target = event.target;
-        if (target && target.matches('a.page-link')) {
-            event.preventDefault(); // Evita el desplazamiento
-            const page = target.getAttribute('data-page');
-            const pageNumber = page.replace(/[()]/g, '')   
-      
-            if (!isNaN(pageNumber)) { // Verifica que la página sea un número válido
-                fetchPage(pageNumber);
-            } else {
-                console.error('Invalid page number:', page);
-            }
-        }
-    });
-
-    // Inicializar la tabla cuando la página se carga
-    fetchData(currentPage);
-});
-
-*/
-
-
 
